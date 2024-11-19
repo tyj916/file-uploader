da26a49cc2166d20a618c4808569367a2afbaeeb
Tan Yi Jun <tanyijun6@gmail.com>
2024-11-19 15:35:13 +0800

    Set up prisma session store

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const session = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { PrismaClient } = require('@prisma/client');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(
+  session({
+    cookie: {
+      maxAge: 14 * 24 * 60 * 60 * 1000,
+    },
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(
+      new PrismaClient(),
+      {
+        checkPeriod: 2 * 60 * 1000,
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }
+    )
+  })
+)
 
 app.get('/', (req, res) => {
   res.send('Hello world');
