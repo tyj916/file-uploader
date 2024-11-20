4a41722ebb3258545801f7943e72e1e138a8b4fc
Tan Yi Jun <tanyijun6@gmail.com>
2024-11-21 00:55:51 +0800

    Add file uploader multer

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 const { Router } = require('express');
 const indexRouter = Router();
+const multer = require('multer');
+const upload = multer({ dest: './uploads/' });
 
 indexRouter.get('/', (req, res) => {
   res.render('index', {
     user: req.user,
   });
+});
+indexRouter.post('/upload', upload.array('uploadedFiles'), (req, res) => {
+  console.log(req.file, req.body);
+  res.redirect('/');
 });
 
 module.exports = indexRouter;
