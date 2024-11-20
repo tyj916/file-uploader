324d0befb7798666edc241ffda217ff65213bb07
Tan Yi Jun <tanyijun6@gmail.com>
2024-11-20 21:14:53 +0800

    Add log out

--- routes/auth.js
+++ routes/auth.js
@@ -53,5 +53,13 @@ authRouter.post(
     failureRedirect: '/log-in',
   })
 );
+authRouter.get('/log-out', (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
 
 module.exports = authRouter;
