b9e04fd46de2c2878ee1c776cf4e5f13ae06738e
Tan Yi Jun <tanyijun6@gmail.com>
2024-11-22 00:45:43 +0800

    Refine upload files form

--- routes/files.js
+++ routes/files.js
@@ -1,11 +1,22 @@
 const { Router } = require('express');
 const filesRouter = Router();
 const multer = require('multer');
-const upload = multer({ dest: './uploads/' });
-
-filesRouter.post('/upload', upload.array('uploadedFiles'), (req, res) => {
-  console.log(req.files);
-  res.redirect('/');
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
 });
+const upload = multer({ storage: storage });
+
+filesRouter.post('/upload', 
+  upload.array('uploadedFiles'), 
+  (req, res) => {
+    console.log(req.files);
+    res.redirect('/');
+  },
+);
 
 module.exports = filesRouter;
