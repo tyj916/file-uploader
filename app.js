17cc790c6227d9cf563d2e09d037cfaf92e3605c
Tan Yi Jun <tanyijun6@gmail.com>
2024-11-21 16:33:20 +0800

    Add css path

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
   cookie: {
